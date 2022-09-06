4881c8a27aced5d5aebe4093b91031db2d721826
Ben Kaminski <ben@benkaminski.com>
2022-09-06 18:28:59 -0400

    started adding data from ACF

--- functions/theme-dependencies.php
+++ functions/theme-dependencies.php
@@ -4,6 +4,7 @@ function enqueue_bkc_scripts() {
     wp_enqueue_script('Bootstrap-5.2.0', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js', array('jquery'), '', true);
     wp_enqueue_script('font-awesome-5', 'https://kit.fontawesome.com/42219cbebc.js', false, null, null, false);
     wp_enqueue_script('bkc-scripts', get_template_directory_uri() . '/assets/js/bkcScripts.min.js', array('jquery'), null, true, null);
+    wp_enqueue_script('Google Maps API', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCtN1uoWQM6pP0PIqEokyf7FRpGPY6kL-U', false, null, null, false);
 }
 add_action('wp_enqueue_scripts', 'enqueue_bkc_scripts');
 
@@ -17,8 +18,37 @@ add_action('wp_enqueue_scripts', 'enqueue_bkc_styles');
 // Register Custom Navigation Walker
 require_once get_template_directory() . '/functions/class-wp-bootstrap-navwalker.php';
 register_nav_menus( array(
-    'primary' => __( 'Primary Menu', 'dmi-theme' ),
-    'secondary_menu' => __('Secondary Menu', 'dmi-theme'),
-    'footer_menu' => __( 'Footer Menu', 'dmi-theme'),  
+    'primary' => __( 'Primary Menu', 'bkc-theme' ),
+    'secondary_menu' => __('Secondary Menu', 'bkc-theme'),
+    'footer_menu' => __( 'Footer Menu', 'bkc-theme'),  
 ));
 
+//REGISTER CUSTOM POST TYPE
+add_action('init', 'register_nuzzo_fam_cpt');
+function register_nuzzo_fam_cpt() {
+    register_post_type('nuzzo_fam', [
+        'label' => 'Nuzzo Family Members',
+        'public' => true, 
+        'capability_type' => 'post',
+        'supports' => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'revisions', 'page-attributes', ),
+        'taxonomies'  => array( 'category' ),
+    ]);
+}
+
+// Add Tag Support
+function nuzzo_fam_tag() {
+    register_taxonomy_for_object_type('post_tag', 'nuzzo_fam_tag');
+}
+add_action('init', 'nuzzo_fam_tag');
+
+
+// GOOGLE MAPS
+function my_acf_google_map_api( $api ){
+
+    $api['key'] = 'AIzaSyCtN1uoWQM6pP0PIqEokyf7FRpGPY6kL-U';
+
+    return $api;
+
+}
+
+add_filter('acf/fields/google_map/api', 'my_acf_google_map_api'); 
--- single.php
+++ single.php
@@ -1,4 +1,36 @@
 <?php get_header(); ?>
+<section class="container" style="padding-top: 5rem;">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+		<h1><?php the_title(); ?></h1>
+		<h4>Birthdate: <?php the_field('birth_date'); ?></h4>
+		
+		<?php $location = get_field('birthplace');
+		if( $location ) {
+		// Loop over segments and construct HTML.
+			$address = '';
+			foreach( array('street_number', 'street_name', 'city', 'state', 'post_code', 'country') as $i => $k ) {
+				if( isset( $location[ $k ] ) ) {
+					$address .= sprintf( '<span class="segment-%s">%s</span>, ', $k, $location[ $k ] );
+				}
+			}
+			// Trim trailing comma.
+			$address = trim( $address, ', ' );
 
+			// Display HTML.
+			echo '<p>' . $address . '.</p>';
+		} ?>
+
+		<?php $location = get_field('birthplace');
+		if( $location ): ?>
+			<div class="acf-map" data-zoom="16">
+				<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+			</div>
+		<?php endif; ?>
+
+	<?php endwhile; else : ?>
+		<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php endif; ?>
+
+</section>
 <?php get_footer(); ?>
